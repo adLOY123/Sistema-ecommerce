8d5714281ddd2b6dd789ea1ad1d879275a825b51
mesi <perezmesiaswiliam@gmail.com>
2026-05-06 07:02:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route }
+  from "react-router-dom"
+  import Home from "./Pages/Home/Home"
+  import Navbar from "./components/layout/Navbar"
+
+function AppContent() {
+  return (
+    <div>
+      <Navbar/>
+      <main className="pt-14 md:pt-20">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+
+        </Routes>
+      </main>
+
+
+
+    </div>
+  )
+
+}
+
+const App = () => {
+  return (
+    <Router>
+        <AppContent/>
+    </Router>
+  )
+}
+
+export default App
